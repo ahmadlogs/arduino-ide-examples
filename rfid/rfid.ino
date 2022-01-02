268c8520da2ee9e45c49cefd495b3a1276bbbc0e
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2022-01-02 11:36:51 +0500

    Add files via upload

---
+++ rfid/rfid.ino
@@ -0,0 +1,113 @@
+
+/************************************************************************************
+ * Created By: Tauseef Ahmad
+ * Created On: December 31, 2021
+ * 
+ * Tutorial: 
+ *
+ * ****************************************************************************
+ * Download Resources
+ * **************************************************************************** 
+ *  Download RFID library:
+ *  https://github.com/miguelbalboa/rfid
+ **********************************************************************************/
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+#include <SPI.h>
+#include <MFRC522.h>
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+#define SS_PIN 10
+#define RST_PIN 9
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+byte readCard[4];
+String MasterTag = "CA59E881";
+String tagID = "";
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// Create instances
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+
+
+/**********************************************************************************************
+ * setup() function
+**********************************************************************************************/
+void setup() 
+{
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  Serial.begin(9600);
+  SPI.begin();
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  mfrc522.PCD_Init();
+  delay(4);
+  //Show details of PCD - MFRC522 Card Reader
+  mfrc522.PCD_DumpVersionToSerial();
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  Serial.println("--------------------------");
+  Serial.println(" Access Control ");
+  Serial.println("Scan Your Card>>");
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+}
+
+
+/**********************************************************************************************
+ * loop() function
+**********************************************************************************************/
+void loop() 
+{
+  //----------------------------------------------------------------------
+  //Wait until new tag is available
+  while (getID()) {
+    //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    if (tagID == MasterTag){
+      Serial.println(" Access Granted!");
+      Serial.println("--------------------------");
+       //You can write any code here like, opening doors, 
+       //switching ON a relay, lighting up an LED etc...
+    }
+    //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    else{
+      Serial.println(" Access Denied!");
+      Serial.println("--------------------------");
+    }
+    //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    delay(2000);
+    Serial.println(" Access Control ");
+    Serial.println("Scan Your Card>>");
+  }
+  //----------------------------------------------------------------------
+}
+
+
+
+
+/**********************************************************************************************
+ * getID() function
+ * Read new tag if available
+**********************************************************************************************/
+boolean getID() 
+{
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  // Getting ready for Reading PICCs
+  //If a new PICC placed to RFID reader continue
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return false;
+  }
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  //Since a PICC placed get Serial and continue
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  return false;
+  }
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  tagID = "";
+  // The MIFARE PICCs that we use have 4 byte UID
+  for ( uint8_t i = 0; i < 4; i++) {
+  //readCard[i] = mfrc522.uid.uidByte[i];
+  // Adds the 4 bytes in a single String variable
+  tagID.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  tagID.toUpperCase();
+  mfrc522.PICC_HaltA(); // Stop reading
+  return true;
+  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+}
