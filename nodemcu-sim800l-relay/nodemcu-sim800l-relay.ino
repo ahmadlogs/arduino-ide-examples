45e8788402be85b42348d3cfa5380d13b5b6359b
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2023-03-25 13:00:54 +0500

    Update nodemcu-sim800l-relay.ino

--- nodemcu-sim800l-relay/nodemcu-sim800l-relay.ino
+++ nodemcu-sim800l-relay/nodemcu-sim800l-relay.ino
@@ -3,8 +3,8 @@
 //sender phone number with country code
 const String PHONE = "Enter_Your_Phone_No";
 
-//GSM Module RX pin to NodeMCU D3
-//GSM Module TX pin to NodeMCU D4
+//GSM Module RX pin to NodeMCU D4
+//GSM Module TX pin to NodeMCU D3
 #define rxPin 2 //D4
 #define txPin 0 //D3
 SoftwareSerial sim800(rxPin,txPin);
